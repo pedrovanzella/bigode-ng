292b85e41bb28e6b6ede74c7d88a8f6393cf4da3
Pedro Vanzella <pedro@pedrovanzella.com>
2012-08-09 15:17:18 -0300

    making sure username is unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
   attr_accessor :login
+  
+  validates :username, presence: true, uniqueness: true
 
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
