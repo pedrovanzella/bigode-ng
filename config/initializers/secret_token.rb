01508c1c07ce58bfcb1a75f946175e4b096dae88
Pedro Vanzella <pedro@pedrovanzella.com>
2012-08-08 22:15:08 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigodeNg::Application.config.secret_token = 'c1e32015193a34eff26e93c6b185da2a3ee1b45cc819932cc8ef58ef4383a61ebf7b44483c747b5f86da60632e6ddba34e275201447daad912c5b1949015def4'
