2b7686dc31e0b3571db022c41e51c2b6381b4a13
Pedro Vanzella <pedro@pedrovanzella.com>
2012-08-11 18:14:03 -0300

    sharedstories controller

--- app/controllers/shared_stories_controller.rb
+++ app/controllers/shared_stories_controller.rb
@@ -1,2 +1,28 @@
 class SharedStoriesController < ApplicationController
+
+  def create
+    @shared_story = SharedStory.create(params)
+    if @shared_story
+      flash[:notice] = "Shared"
+    else
+      flash[:error] = "Could not share"
+    end
+
+    redirect_to :back
+  end
+
+  def destroy
+    @shared_story = SharedStory.find_by_user_id_and_story_id(params[:user_id], params[:story_id])
+    if @shared_story
+      @shared_story.destroy
+      flash[:notice] = "Unshared"
+    else
+      flash[:error] = "Could not unshare (maybe this story wasn't shared?)"
+    end
+    redirect_to :back
+  end
+
+  def index
+    @shared_stories = SharedStory.order("created_at DESC").find_all_by_user_id(params[:user_id])
+  end
 end
