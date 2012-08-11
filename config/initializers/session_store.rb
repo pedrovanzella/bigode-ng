051c96ddfbf2bd3f852fea519b06c21a9908732c
Pedro Vanzella <pedro@pedrovanzella.com>
2012-08-11 18:03:17 -0300

    updated to rails 3.2.8

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-BigodeNg::Application.config.session_store :cookie_store, key: '_bigode-ng_session'
+BigodeNg::Application.config.session_store :cookie_store, key: '_bigode_ng_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
